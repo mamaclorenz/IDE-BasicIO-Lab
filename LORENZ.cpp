5315731c21051b811261b45bdcc1cc9fc48598ce
mamaclorenz <mamaclorenz29@gmail.com>
2025-08-08 21:14:33 +0800

    Update LORENZ.cpp

--- LORENZ.cpp
+++ LORENZ.cpp
@@ -7,7 +7,6 @@ int main() {
     string fullName;
     int age, currentYear, yearTurn60;
 
-    // Input section
     cout << "Enter your full name: ";
     getline(cin, fullName);
 
@@ -17,10 +16,8 @@ int main() {
     cout << "Enter the current year: ";
     cin >> currentYear;
 
-    // Calculation
     yearTurn60 = currentYear + (60 - age);
 
-    // Output section
     cout << "\nHello, " << fullName << "! You are " << age << " years old.\n";
     cout << "You will turn 60 years old in the year " << yearTurn60 << "." << endl;
 
